98956dca173f7bd7a940f2499aafc7ab30aaa0b5
Lucifer <phannh.uetk58@gmail.com>
2017-08-21 16:18:14 +0700

    add clock and authenticate by facebook

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,8 @@ Rails.application.routes.draw do
   resources :posts
   devise_for :users, path: 'auth', path_names: { sign_in: 'login',
     sign_out: 'logout', password: 'secret', confirmation: 'verification',
-    unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
+    unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' },
+    :controllers => { omniauth_callbacks: "omniauth_callbacks" }
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
